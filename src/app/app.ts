7535aebbd9ef284a98c7aeeba7c197a666e58ca1
Risk Solution <risksolution25@gmail.com>
2025-12-15 13:31:44 +0530

    15-dec-25 13:31 commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angularcicd updated on 15-12-25 13:15 pm');
+  protected readonly title = signal('angularcicd updated on 15-12-25 13:31 pm');
 }
